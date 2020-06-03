ff2131728b961eef025533c910788691ccebb488
chenyaowei <754126330@qq.com>
2020-06-03 13:24:06 +0800

    第二次提交，登录界面

--- app/src/main/java/com/test/demo02/MainActivity.java
+++ app/src/main/java/com/test/demo02/MainActivity.java
@@ -2,13 +2,34 @@ package com.test.demo02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_login1,btn_login2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login1=findViewById(R.id.login1);
+        btn_login2=findViewById(R.id.button);
+        btn_login1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Login1Activity.class);
+                startActivity(intent);
+            }
+        });
+        btn_login2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Login2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
